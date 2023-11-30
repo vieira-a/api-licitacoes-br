4b62b63734fa194ee12ef8dea1d970ae6c083e3a
Anderson Vieira <asvieira.br@gmail.com>
2023-11-30 10:34:33 -0300

    docs: create FetchItemProcessController documentation

--- src/modules/process-item/controllers/fetch-item-process.controller.ts
+++ src/modules/process-item/controllers/fetch-item-process.controller.ts
@@ -2,13 +2,27 @@ import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { SaveItemProcessService } from '../services/save-item-process.service';
 import { Response } from 'express';
 import { serverError } from '../../../shared/exceptions/server-error';
+import {
+  ApiTags,
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+} from '@nestjs/swagger';
 
-@Controller('/fetch-process-itens')
+@Controller('/extract-items')
+@ApiTags('extração de dados')
 export class FetchItemProcessController {
   constructor(
     private readonly saveItemProcessService: SaveItemProcessService,
   ) {}
   @Get()
+  @ApiOkResponse({
+    status: HttpStatus.OK,
+    description: 'Extração de itens de processos executada com sucesso',
+  })
+  @ApiInternalServerErrorResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Houve uma falha ao salvar itens dos processos',
+  })
   async fetchProcessItem(@Res() res: Response) {
     try {
       await this.saveItemProcessService.saveItemProcess();
