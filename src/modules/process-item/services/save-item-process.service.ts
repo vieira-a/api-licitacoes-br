289d2b4fec170bdb91c0e21b9332e8874609a9c2
Anderson Vieira <asvieira.br@gmail.com>
2023-11-30 17:01:44 -0300

    feat: ensure SaveItemProcessService runs at spefific times

--- src/modules/process-item/services/save-item-process.service.ts
+++ src/modules/process-item/services/save-item-process.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ItemProcessRepository } from '../repositories/process-item.repository';
 import { FetchItemProcessService } from './fetch-item-process.service';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class SaveItemProcessService {
@@ -9,6 +10,7 @@ export class SaveItemProcessService {
     private readonly fetchItemProcessService: FetchItemProcessService,
   ) {}
 
+  @Cron('0 7,13,19,1 * * *')
   async saveItemProcess() {
     const itemProcess = await this.fetchItemProcessService.fetchItemProcess();
 
